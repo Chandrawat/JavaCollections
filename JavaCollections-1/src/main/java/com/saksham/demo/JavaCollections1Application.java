2fefc49079893c406fca475e35d2899ce875c205
Saksham Chandrawat <saksham.mist@gmail.com>
2024-07-01 17:24:19 -0500

    first commit

---
+++ JavaCollections-1/src/main/java/com/saksham/demo/JavaCollections1Application.java
@@ -0,0 +1,13 @@
+package com.saksham.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollections1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCollections1Application.class, args);
+	}
+
+}
---
+++ JavaCollections-1/src/test/java/com/saksham/demo/JavaCollections1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksham.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCollections1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
