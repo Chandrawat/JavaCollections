12e6d3f1cdf40d8a40e1e444085bedfff8cd0844
Saksham Chandrawat <saksham.mist@gmail.com>
2024-09-08 14:25:44 -0500

    arrays

---
+++ JavaCollections-1/src/main/java/com/saksham/array/ArraysUdemy.java
@@ -0,0 +1,14 @@
+package com.saksham.array;
+
+public class ArraysUdemy {
+
+	public static void main(String[] args) {
+		int[] arr = new int[7];
+		
+		arr[0] = 20;
+		
+		System.out.println(arr[0]);
+
+	}
+
+}
--- JavaCollections-1/src/main/java/com/saksham/demo/JavaCollections1Application.java
+++ JavaCollections-1/src/main/java/com/saksham/demo/JavaCollections1Application.java
@@ -8,6 +8,13 @@ public class JavaCollections1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaCollections1Application.class, args);
+		
+		int[] arr = new int[7];
+		
+		arr[0] = 20;
+		
+		System.out.println(arr[0]);
+		
 	}
 
 }
